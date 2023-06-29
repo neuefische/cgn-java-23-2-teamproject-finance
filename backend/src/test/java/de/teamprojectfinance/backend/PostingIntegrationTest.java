1f360a025ee52f35866f423c8ee3671bdf621efd
christophgroneberg <christophgroneberg1@gmx.de>
2023-06-29 17:51:38 +0200

    improve Unit Test WhenAddingPosting_ThenReturnPosting()

--- backend/src/test/java/de/teamprojectfinance/backend/PostingIntegrationTest.java
+++ backend/src/test/java/de/teamprojectfinance/backend/PostingIntegrationTest.java
@@ -1,6 +1,6 @@
 package de.teamprojectfinance.backend;
 
-import org.assertj.core.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,10 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
