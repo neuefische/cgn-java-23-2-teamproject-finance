f6c3c0a57a83fc1038e7cff4d71911a7fbb0a155
christophgroneberg <christophgroneberg1@gmx.de>
2023-06-29 11:15:49 +0200

    create AddPosting.tsx, AddPosting.css, implement handleSubmit method in frontend module

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,24 @@
 
 import './App.css'
-import {FormEvent, useState} from "react";
-import {Posting} from "./model/model.ts";
+
+import AddPosting from "./addPosting/AddPosting.tsx";
+
 
 export default function App() {
 
-    const[input, setInput] = useState<Posting>()
-    const[description, setDescription]= useState<string>("")
-    const[amount, setAmount] = useState<number>(0)
 
-    function handleSubmit(event: FormEvent<HTMLFormElement>){
-        event.preventDefault()
-        setInput({
-            id: "01",
-            description: description,
-            amount: amount,
-            category: "income"
-        }
-    )
-       
-    }
 
-    function addVisitor(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault()
-        setVisitors([...visitors, newVisitor])
-        setNewVisitor({
-            id: "",
-            "firstName": "",
-            "lastName": "",
-        })
-    }
+
+
+
+
 
   return (
       <>
-          <form onSubmit={handleSubmit}>
-              <h1>Finanzen virtuelles Tierheim</h1>
-
-              <input type={"text"} onChange={event => setDescription(event.target.value)} value={description} placeholder={"description"}/>
-              <input type={"number"} onChange={event => setAmount(parseInt(event.target.value))} value={amount} placeholder={"amount"}/>
-              <button>Add Posting</button>
-
-          </form>
+          <h1>Finanzen virtuelles Tierheim</h1>
 
 
+          <AddPosting></AddPosting>
 
       </>
 
---
+++ frontend/src/addPosting/AddPosting.tsx
@@ -0,0 +1,44 @@
+import {FormEvent, useState} from "react";
+import axios from "axios";
+
+
+
+
+
+export default function AddPosting(){
+
+    const[description, setDescription]= useState<string>("")
+    const[amount, setAmount] = useState<number>()
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>){
+        event.preventDefault()
+        axios.post(
+            "/api/finance",{
+                "description": description,
+                "amount": amount
+            }
+        ).catch(console.error)
+        setAmount(0)
+        setDescription("")
+
+
+
+    }
+    return(
+        <>
+            <form onSubmit={handleSubmit}>
+
+
+                <input type={"text"} onChange={event => setDescription(event.target.value)} value={description} placeholder={"description"}/>
+                <input type={"number"} onChange={event => setAmount(parseInt(event.target.value))} value={amount} placeholder={"amount"}/>
+                <button>Add Posting</button>
+
+
+
+            </form>
+
+
+        </>
+
+    )
+}
