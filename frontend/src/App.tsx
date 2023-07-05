924d9c2015382c1112496364c4534e706741550d
Ben-21 <bs@schaefer-inet.de>
2023-07-05 18:11:39 +0200

    add UpdateTransaction in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,24 +3,36 @@ import {FormEvent, useEffect, useState} from "react";
 import axios from "axios";
 import {Transaction} from "./model/model.ts";
 import TransactionCollection from "./TransactionCollection/TransactionCollection.tsx";
+import UpdateTransaction from "./UpdateTransaction/UpdateTransaction.tsx";
 
 
 export default function App() {
 
 
-    const [description, setDescription] = useState<string>("")
-    const [amount, setAmount] = useState<number>(0)
+    const [description, setDescription] = useState<string>("");
+    const [amount, setAmount] = useState<number>(0);
     const [category, setCategory] = useState<"INCOME" | "EXPENSE">("INCOME");
-    const [transactions,setTransactions] = useState<Transaction[]>([])
+    const [id, setId] = useState<string>("")
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+    const [updateTransactionVisibility, setUpdateTransactionVisibility] = useState(false);
+    //  const [selectedDescription, setSelectedDescription] = useState<string>("")
+    //const [selectedAmount, setSelectedAmount] = useState<number>(0)
+    //const [selectedCategory, setSelectedCategory] = useState<"INCOME"|"EXEPNSE">()
 
-    function loadTransactions(){
+
+    function loadTransactions() {
         axios.get(
             "/api/finance/")
             .catch(console.error)
-            .then(response=>{setTransactions(response?.data)})
+            .then(response => {
+                setTransactions(response?.data)
+            })
 
     }
-    useEffect(loadTransactions,[transactions])
+
+    useEffect(loadTransactions, [])
+ 
+
     function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
 
@@ -33,19 +45,56 @@ export default function App() {
         ).then(() => {
                 setAmount(0)
                 setDescription("")
-            setCategory("INCOME")
+                setCategory("INCOME")
             }
         ).catch(console.error)
+    }
+
+    function initializeUpdateComponent(transactionId: string) {
+
+        const selectedTransaction: Transaction | undefined = transactions.find(item => item.id === transactionId)
+
+        if (selectedTransaction !== undefined) {
+            setDescription(selectedTransaction.description)
+            setAmount(selectedTransaction.amount)
+            setCategory(selectedTransaction.category)
+            setId(selectedTransaction.id)
+            setUpdateTransactionVisibility(!updateTransactionVisibility);
+        } else {
+            throw DOMException
+        }
+
 
     }
 
+    function cancelUpdateComponent() {
+        setUpdateTransactionVisibility(!updateTransactionVisibility);
+    }
+
+    function handleUpdate(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        axios.put(
+            "/api/finance", {
+                "id": id,
+                "description": description,
+                "amount": amount,
+                "category": category
+            }).then(() => {
+                setId("")
+                setAmount(0)
+                setDescription("")
+                setCategory("INCOME")
+            }
+        ).then (()=>loadTransactions())
+
+    }
 
 
     return (
         <>
             <h1>Finanzen virtuelles Tierheim</h1>
 
-            <TransactionCollection transaction={transactions}/>
+            <TransactionCollection transaction={transactions} update={initializeUpdateComponent}/>
             <AddTransaction submit={handleSubmit} setAmount={setAmount} setDescription={setDescription} amount={amount}
                             description={description} category={category} setCategory={setCategory}/>
 
--- frontend/src/TransactionCard/TransactionCard.tsx
+++ frontend/src/TransactionCard/TransactionCard.tsx
@@ -2,6 +2,7 @@ import {Transaction} from "../model/model.ts";
 
 type Props = {
     transaction: Transaction,
+    update: () => void,
 }
 export default function TransactionCard(props: Props) {
 
@@ -11,6 +12,10 @@ export default function TransactionCard(props: Props) {
             <h3> Beschreibung: {props.transaction.description}</h3>
             <p> Betrag: {props.transaction.amount}</p>
             <p> Kategorie: (Income/Expense): {props.transaction.category}</p>
+            <button onClick={props.update}>Bearbeiten</button>
+
+
+
         </>
     )
 }
