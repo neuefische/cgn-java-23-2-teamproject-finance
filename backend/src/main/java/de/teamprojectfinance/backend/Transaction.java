0eae83a89725d8c8e49e77dc86ab98617bcbeb83
Ben-21 <bs@schaefer-inet.de>
2023-07-05 15:30:41 +0200

    add Spring Data and MongoDB

--- backend/src/main/java/de/teamprojectfinance/backend/Transaction.java
+++ backend/src/main/java/de/teamprojectfinance/backend/Transaction.java
@@ -3,13 +3,17 @@ package de.teamprojectfinance.backend;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document("transactions")
 public class Transaction {
 
+    @Id
     private String id;
 
     private String description;
--- backend/src/main/java/de/teamprojectfinance/backend/TransactionRepo.java
+++ backend/src/main/java/de/teamprojectfinance/backend/TransactionRepo.java
@@ -1,25 +1,13 @@
 package de.teamprojectfinance.backend;
 
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+
 
 @Repository
-public class TransactionRepo {
+public interface TransactionRepo extends MongoRepository<Transaction, String> {
 
-    private final List<Transaction> postingModels;
 
-    public TransactionRepo(List<Transaction> postingModels){
-        this.postingModels = postingModels;
-    }
-
-    public Transaction addPosting(Transaction postingModel) {
-        this.postingModels.add(postingModel);
-        return postingModel;
-    }
-
-    public List<Transaction> getAllTransactions() {
-        return postingModels;
-    }
 }
