da6cd18981ff439927c112e717e1796261e76e20
Ben-21 <bs@schaefer-inet.de>
2023-07-08 11:41:02 +0200

    first version of TransactionCollection

--- frontend/src/TransactionCollection/TransactionCollection.tsx
+++ frontend/src/TransactionCollection/TransactionCollection.tsx
@@ -1,18 +1,24 @@
 import {Transaction} from "../model/model.ts";
 import TransactionCard from "../TransactionCard/TransactionCard.tsx";
+import "./TransactionCollection.css";
 
-type Props={
+type Props = {
     transaction: Transaction[]
     update: (transactionId: string) => void,
 }
 
-export default function TransactionCollection(props: Props){
-    return(
+export default function TransactionCollection(props: Props) {
+    return (
         <>
-            <h2>Buchungsliste</h2>
-            {
-                props.transaction.map((transaction )=><TransactionCard transaction={transaction} key={transaction.id} update={()=>props.update(transaction.id) }/>)
-            }
+            <div className={"transactionCollectionContainer"}>
+                <h2>Buchungsliste</h2>
+                {
+                    props.transaction.map((transaction) => <TransactionCard
+                        transaction={transaction}
+                        key={transaction.id}
+                        update={() => props.update(transaction.id)}/>)
+                }
+            </div>
         </>
     )
 }
