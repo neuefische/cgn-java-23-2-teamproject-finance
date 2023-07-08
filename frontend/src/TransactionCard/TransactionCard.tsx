e62f908b8340868f053f1ffbb08065d7011495f6
Ben-21 <bs@schaefer-inet.de>
2023-07-08 11:39:26 +0200

    first version of TransactionCard Design

--- frontend/src/TransactionCard/TransactionCard.tsx
+++ frontend/src/TransactionCard/TransactionCard.tsx
@@ -1,21 +1,36 @@
-import {Transaction} from "../model/model.ts";
+import { Transaction } from "../model/model.ts";
+import { Box, Typography } from "@mui/material";
+import "./TransactionCard.css";
 
 type Props = {
-    transaction: Transaction,
-    update: () => void,
-}
+    transaction: Transaction;
+    update: () => void;
+};
+
 export default function TransactionCard(props: Props) {
-
-
     return (
-        <>
-            <h3> Beschreibung: {props.transaction.description}</h3>
-            <p> Betrag: {props.transaction.amount}</p>
-            <p> Kategorie: (Income/Expense): {props.transaction.category}</p>
-            <button onClick={props.update}>Bearbeiten</button>
 
+        <fieldset className={"transactionFieldset"}>
+            <legend>DATUM</legend>
+            <Box className="transactionCard">
 
+            <div className="row">
+                <Typography className="label">Beschreibung:</Typography>
+                <Typography className="value">{props.transaction.description}</Typography>
+            </div>
+            <div className="row">
+                <Typography className="label">Betrag:</Typography>
+                <Typography className="value">{props.transaction.amount}</Typography>
+            </div>
+            <div className="row">
+                <Typography className="label">Kategorie (Income/Expense):</Typography>
+                <Typography className="value">{props.transaction.category}</Typography>
+            </div>
+            <button className="button" onClick={props.update}>
+                Bearbeiten
+            </button>
 
-        </>
-    )
+        </Box>
+        </fieldset>
+    );
 }
