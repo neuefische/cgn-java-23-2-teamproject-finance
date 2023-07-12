373e82e75ce9b903a4881766b68475ed77e424d2
christophgroneberg <christophgroneberg1@gmx.de>
2023-07-12 18:06:09 +0200

    add MongoUserIntegrationTest.java and delete get-endpoint "me"

--- backend/src/main/java/de/teamprojectfinance/backend/security/MongoUserController.java
+++ backend/src/main/java/de/teamprojectfinance/backend/security/MongoUserController.java
@@ -6,19 +6,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
+
 
 @RestController
 @RequestMapping("/api/users")
 public class MongoUserController {
 
-    @GetMapping("me")
-    public String getUserInfo(Principal principal){
-        if (principal == null){
-            return "anonymousUser";
-        }
-        return principal.getName();
-    }
 
     @GetMapping("me2")
     public String getUserInfo2(){
